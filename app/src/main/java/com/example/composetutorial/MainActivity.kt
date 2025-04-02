052874c5629b5fb7f3176f8b0486685eff35b408
Pickle Ryskin <pickle@Pickles-MacBook-Pro.local>
2025-04-02 20:55:35 +0400

    Initial commit

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PreviewMessageCard()
+        }
+    }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello, $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard("Android")
+    }
+}
