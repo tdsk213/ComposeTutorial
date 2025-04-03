c77eaac281c5053b8ea9749a022f1ca535d6a7b0
tdsk213 <tdsk213@ya.ru>
2025-04-03 12:36:35 +0400

    2.Layouts

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -17,14 +29,33 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    data class Message(val author: String, val message: String)
+
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "Hello, $name!")
+    fun MessageCard(msg: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column {
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(text = msg.author)
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(text = msg.message)
+            }
+        }
     }
 
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard("Android")
+        MessageCard(msg = Message("Vitalik", "Feed me!"))
     }
 }
