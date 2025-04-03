ff54a9a030e725f80fa9af146f86c97c38f433b3
tdsk213 <tdsk213@ya.ru>
2025-04-03 13:37:46 +0400

    3.Material Design

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,17 +24,23 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewMessageCard()
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Vitalik", "Jetpack Compose"))
+                }
+            }
+//            PreviewMessageCard()
         }
     }
 
-    data class Message(val author: String, val message: String)
+    data class Message(val author: String, val body: String)
 
     @Composable
     fun MessageCard(msg: Message) {
@@ -40,22 +51,48 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
 
             Spacer(modifier = Modifier.width(10.dp))
 
             Column {
-                Spacer(modifier = Modifier.height(2.dp))
-                Text(text = msg.author)
-                Spacer(modifier = Modifier.height(2.dp))
-                Text(text = msg.message)
+
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodySmall)
+
+                }
             }
         }
     }
 
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+
+
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(msg = Message("Vitalik", "Feed me!"))
+
+        ComposeTutorialTheme {
+            Surface {
+                MessageCard(msg = Message("Vitalik", "Feed me!"))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/composetutorial/ui/theme/Color.kt
+++ app/src/main/java/com/example/composetutorial/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.composetutorial.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val Purple80 = Color(0xFFCEC1FA)
+val PurpleGrey80 = Color(0xFFA198AD)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey40 = Color(0xFF494659)
 val Pink40 = Color(0xFF7D5260)
